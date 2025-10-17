2bdda2c332117e6dd8209b2d49c9fc925541e056
aryabilal <aryaarchilles@gmail.com>
2025-10-17 16:23:25 +0700

    tes

--- src/app/tes/page.tsx
+++ src/app/tes/page.tsx
@@ -24,30 +24,10 @@ import Image from "next/image";
 
 // app/page.tsx
 export default async function Tes() {
-  const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://env-6771182.jh-beon.cloud";
-
-  const res = await fetch(`${API_URL}/api/data`, {
-    cache: "no-store",
-  });
-
-  if (!res.ok) {
-    throw new Error("Gagal fetch data dari backend");
-  }
-
-  // backend mengirim array langsung, bukan object dengan 'items'
-  const data: { id: number; name: string }[] = await res.json();
 
   return (
     <div>
       <h1>Data dari Go Backend</h1>
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>
-            {item.id}. {item.name}
-          </li>
-        ))}
-      </ul>
-
       <Image src="remote-data/fotobgmerah.jpg" alt="foto" width={120} height={20}></Image>
     </div>
   );
